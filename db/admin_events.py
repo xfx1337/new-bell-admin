12615fe64e20b0fd15e1d39a2d9711acb2099658
xfx1337 <tttfff1338@gmail.com>
2023-06-28 18:50:49 +0500

    admin_event read all fix

--- db/admin_events.py
+++ db/admin_events.py
@@ -22,7 +22,7 @@ def close(id):
         if id == "all":
             cursor.execute(f"""
             DELETE FROM admin_events
-            """, [id])
+            """)
             connection.commit()
             return 0
 
